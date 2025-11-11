170cbe00184979da2995dd9ced85f3884a7f5a02
rafal.duluk <rafal.duluk@intiaro.com>
2025-11-11 12:37:42 +0100

    Initial commit - workshop stage 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Workshop – React State & Redux</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
